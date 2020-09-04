a23767a74458ff79bcb131e3712c95973475f79d
Yadunand Kamath <yadu10a2000@gmail.com>
2020-09-04 16:22:53 +0530

    Create conf.py

---
+++ NetworkScanner/conf.py
@@ -0,0 +1,6 @@
+""" Configuration File to send output via Bot to Telegram Channel """ 
+
+bolt_api_key = "XXXX"        # Your Bolt Cloud API Key
+device_id = "XXXX"           # Your device ID [BOLTXXXX where XXXX are some numbers]
+telegram_chat_id = "@XXXX"   # Channel name of Telegram channel. Paste after @ symbol.
+telegram_bot_id = "botXXXX"  # Bot Token ID of Telegram Bot. Paste after 'bot' text.
